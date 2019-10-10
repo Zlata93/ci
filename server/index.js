8db54e8036fc26eb32dcdcfe832b69a53b048165
Zlata Kotlova <z.kotlova@informika.ru>
2019-10-10 19:44:42 +0300

    basic server and interface

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
+
+const app = express();
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 9000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express is running on port ' + app.get('port'));
+});
