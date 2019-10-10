bbc58b5e1ec289ea02bf02f78afcccd76e6e8810
Zlata Kotlova <z.kotlova@informika.ru>
2019-10-10 20:12:33 +0300

    form

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const parser = require('body-parser');
 const handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
 
 const app = express();
@@ -9,9 +10,16 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 9000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(parser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.post('/build', (req, res) => {
+    console.log(`Commit hash: ${req.body.commit_hash}`);
+    console.log(`Build command: ${req.body.build_command}`);
+    res.redirect(303, '/');
 });
 
 app.use((req, res) => {
